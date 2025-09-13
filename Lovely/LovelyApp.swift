10246aa993630c4e437755631f2804e25eca1863
Andy Ren <andyren33@gmail.com>
2025-09-12 22:05:56 -0700

    Initial Commit

---
+++ Lovely/LovelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovelyApp.swift
+//  Lovely
+//
+//  Created by Andy Ren on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LovelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
